52e4d90049c7e5b42bf406b49dbf41df1dd9cd6c
Ivcota <ivcotad@gmail.com>
2021-10-25 14:58:59 -0700

    Validation has been created for one api route

---
+++ README.md
@@ -0,0 +1,7 @@
+# Simple API that connects to the Automatic Leads Database.
+
+Environment Variable Required:
+
+- PORT
+- APIKEY
+- DATABASE_URL
--- src/routes/tblInterviewCold.ts
+++ src/routes/tblInterviewCold.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import { getInterviewsColds } from "../controllers/tblInterviewCold";
+import { query } from "express-validator";
+import { tblInterviewColdValidator } from "../validators/tblInterviewValidators";
 const router = express.Router();
 
-router.route('/').get(getInterviewsColds)
+router.route("/").get(tblInterviewColdValidator, getInterviewsColds);
 
-export {router as interviewColdRoutes}
+export { router as interviewColdRoutes };
