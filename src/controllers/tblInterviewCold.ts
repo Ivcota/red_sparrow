37b4c3b5c8cb2e339115a8992616678c8b2271da
Ivcota <ivcotad@gmail.com>
2021-10-25 14:07:31 -0700

    Added filters

--- src/controllers/tblInterviewCold.ts
+++ src/controllers/tblInterviewCold.ts
@@ -2,12 +2,23 @@ import { Request, Response } from "express";
 import { db } from "../db/db";
 
 export const getInterviewsColds = async (req: Request, res: Response) => {
-  const { contractor } = req.query;
+  let { contractor, date } = req.query;
 
+  contractor ? contractor : (contractor = "");
+  date ? date : (date = "1965-01-01");
+  
   const leads = await db.tblInterviewCold.findMany({
     where: {
       Contractor_AFF: {
         equals: contractor as string,
+      },
+      TimeSlot: {
+        contains: "a",
+      },
+      AND: {
+        DateEdited: {
+          gte: new Date(date as string),
+        },
       },
     },
     select: {
