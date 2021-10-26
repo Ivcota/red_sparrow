fc412b9e7759cedd5b968da19cce1ce70a13eff0
Ivcota <ivcotad@gmail.com>
2021-10-25 18:30:51 -0700

    Added cors

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import { homeRoutes } from "./routes/home";
 import { interviewColdRoutes } from "./routes/tblInterviewCold";
 import { agentsRoutes } from "./routes/agents";
+import cors from "cors";
 dotenv.config();
 
 const PORT = process.env.PORT;
@@ -10,11 +11,12 @@ const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
 // Routes
 app.use("", homeRoutes);
 app.use("/api/interview-cold", interviewColdRoutes);
-app.use('/api/agents', agentsRoutes)
+app.use("/api/agents", agentsRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}.`);
